3bd4ffb3939e2e367100e6480c521f29ae5dfa30
Pablo Bezerra <pablobzerrads@gmail.com>
2025-07-21 14:40:08 -0300

    Create Exemplo.java

---
+++ Java/basico/Exemplo.java
@@ -0,0 +1,10 @@
+package java.basico;
+
+import java.basico.enums.EstadosBrasileiros;
+
+class Exemplo {
+    public static void main(String[] args) {
+        EstadosBrasileiros eb = EstadosBrasileiros.SAO_PAULO;
+
+    }
+}
